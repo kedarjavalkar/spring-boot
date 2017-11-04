d9e4d1b69686a40911b90293177b5840147c6871
kedar <kedar.javalkar@gmail.com>
2017-11-04 21:33:09 +0530

    Updating Log format. Adding banner.txt
    adding APIs
     - get by id
     - get by first name and last name
     - get all

--- src/main/java/com/example/demo/controller/DemoController.java
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -1,9 +1,16 @@
 package com.example.demo.controller;
 
+import java.util.List;
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,9 +29,23 @@ public class DemoController {
 	}
 
 	@RequestMapping(value = "/user", method = RequestMethod.POST)
-	public @ResponseBody User saveUser(@RequestBody User user) {
-		System.err.println("saving user");
-		return userRepository.save(user);
+	public @ResponseBody Response saveUser(@RequestBody User user) {
+		return Response.status(Status.CREATED).entity(userRepository.save(user)).build();
 	}
 
+	@RequestMapping(value = "/user/{id}", method = RequestMethod.GET)
+	public @ResponseBody User getUserById(@PathVariable("id") Long id) {
+		return userRepository.getById(id);
+	}
+
+	@RequestMapping(value = "/users", method = RequestMethod.GET)
+	public @ResponseBody List<User> getUsers() {
+		return userRepository.getAll();
+	}
+
+	@RequestMapping(value = "/users/find", method = RequestMethod.GET)
+	public @ResponseBody List<User> getUserByIdAndFirstName(@RequestParam("firstName") String firstName,
+			@RequestParam("lastName") String lastName) {
+		return userRepository.getByFirstNameAndLastName(firstName, lastName);
+	}
 }
--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -1,5 +1,8 @@
 package com.example.demo.repository;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,5 +12,10 @@ import com.example.demo.entity.User;
 public interface UserRepository extends CrudRepository<User, Long> {
 
 	public User getById(Long id);
+
+	@Query("SELECT u FROM User as u")
+	public List<User> getAll();
+
+	public List<User> getByFirstNameAndLastName(String firstName, String lastName);
 
 }
